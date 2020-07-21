c5246d8161181fce37285d2b90f4b8bae3156c68
Sumera Priyadarsini <sylphrenadin@gmail.com>
2020-07-21 18:37:32 +0530

    Add label flag

--- main.go
+++ main.go
@@ -16,23 +16,22 @@ type issueConfig struct {
 
 func parseArgs(args []string) *issueConfig {
 	return &issueConfig{
-		ownerName:  string(args[0]),
-		repoName:   string(args[1]),
-		issueLabel: string(args[2]),
+		ownerName: string(args[0]),
+		repoName:  string(args[1]),
 	}
 }
 
-func getIssue(issueInfo *issueConfig, limit int) {
+func getIssue(issueInfo *issueConfig, limit int, issueLabel string) {
 
 	ctx := context.Background()
 	client := github.NewClient(nil)
 	count := 1
 
 	issList, _, _ := client.Issues.ListByRepo(ctx, issueInfo.ownerName, issueInfo.repoName, nil)
-    	//val := issList[0]
+	//val := issList[0]
 	for _, issue := range issList {
 		for _, label := range issue.Labels {
-			if label.GetName() == issueInfo.issueLabel && count <= limit {
+			if label.GetName() == issueLabel && count <= limit {
 				fmt.Println(issue.GetNumber(), "\t\t", issue.GetURL())
 				count++
 			}
@@ -46,13 +45,14 @@ func main() {
 	//fmt.Println("Format: fetchin -[owner name] -[repo name] -[issue label]")
 
 	limit := flag.Int("limit", 10, "maximum number of results")
+	label := flag.String("label", "beginner", "issue label")
 	flag.Parse()
 
 	info := flag.Args()
 
 	issueInfo := parseArgs(info)
 
-	getIssue(issueInfo, *limit)
+	getIssue(issueInfo, *limit, *label)
 
-	fmt.Println("limit:", *limit)
+	fmt.Println("limit:", *limit, *label)
 }
